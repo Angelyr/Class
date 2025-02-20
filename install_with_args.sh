440cbaa50c864022cdaeaaf6d9ddface3c491564
Angelyr <scardking@gmail.com>
2025-02-20 14:59:02 -0500

    export cmakeprefixpath

--- install_with_args.sh
+++ install_with_args.sh
@@ -11,7 +11,7 @@ cmake -S $dir/Catch2 -B $dir/build-Catch2 \
   -DCMAKE_INSTALL_PREFIX=$dir/build-Catch2/install/
 cmake --build $dir/build-Catch2 -j8 --target install
 
-CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$dir/build-Catch2/install
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$dir/build-Catch2/install
 
 # Build Class
 rm $dir/build-class -rf
